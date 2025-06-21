d648520883ddf3551af8b930cac4c09c6d424778
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2025-06-21 16:54:06 +0600

    BookShema Model Added

---
+++ src/app/models/books.model.ts
@@ -0,0 +1,49 @@
+import { model, Schema } from "mongoose";
+
+
+
+const bookSchema = new Schema({
+    title:{
+        type: String,
+        required: true
+    },
+    author:{
+        type: String,
+        required: true
+    },
+    genre:{
+        type: String,
+        enum: ['FICTION', 'NON-FICTION', 'SCIENCE', 'HISTORY', 'BIOGRAPHY', 'FANTASY'],
+        required: true
+
+    },
+    isbn:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    description:{
+        type: String,
+        
+    },
+    copies:{
+        type: Number,
+        required: true,
+        min: 0
+    },
+    available:{
+        type: Boolean,
+        Defaults : true,
+        
+    },
+     
+     
+}, {
+    timestamps: true,});
+
+    bookSchema.methods.updateAvailability = function (this: any) {
+    this.available = this.copies > 0
+    return this.save();
+};
+
+export const Book = model('Book', bookSchema);
