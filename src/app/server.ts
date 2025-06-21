7d1d5f0aa3d10ff8c764d26ee5a9fe4bc03b66b0
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2025-06-21 15:19:32 +0600

    Successfully Connect with MongoDB

--- src/app/server.ts
+++ src/app/server.ts
@@ -1,7 +1,29 @@
 import express from 'express';
 import cors from 'cors';
+import mongoose from 'mongoose';
+import config from '../config';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
+
+const PORT = config.PORT 
+async function main() {
+  try {
+    await mongoose.connect(config.DATABASE_URL as string)
+    console.log('MongoDB connected');
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);    
+    });
+  } catch (err) {
+    console.error('Failed to connect to MongoDB', err);
+  }
+}
+
+main();
