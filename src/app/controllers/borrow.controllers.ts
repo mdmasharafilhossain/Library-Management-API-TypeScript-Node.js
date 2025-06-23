fe55e5cb9fe999c1a8ae1cce47ee4b09b18e5232
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2025-06-23 14:34:56 +0600

    borrow colntroller functionality added

--- src/app/controllers/borrow.controllers.ts
+++ src/app/controllers/borrow.controllers.ts
@@ -23,3 +23,44 @@ generalResponse(res, 200, 'Book borrowed successfully', borrow);
     PassError(error);
   }
 });
+
+BorrowRoute.get("/api/borrow", async (req:Request, res:Response , PassError:NextFunction) => {
+  try {
+   const BorrowdBooksSummary = await Borrow.aggregate([
+   {
+    $group: {
+      _id: '$book',
+      totalQuantity: {$sum: '$quantity'}
+    },
+    
+   },
+   {
+    $lookup: {
+        from: 'books',
+        localField: '_id',
+        foreignField: '_id',  
+        as: 'bookDetails' 
+      }
+    },
+    {
+      $unwind: '$bookDetails'
+    },
+    {
+      $project: {
+        _id: 0,
+        book:{
+          title: '$bookDetails.title',
+          isbn: '$bookDetails.isbn'
+        },
+         totalQuantity: 1
+      }
+    }
+
+
+   ]);
+
+   generalResponse(res, 200, 'Borrowed books summary retrieved successfully', BorrowdBooksSummary)
+  } catch (error) {
+    PassError(error);
+  }
+});
--- src/app/interfaces/book.interfaces.ts
+++ src/app/interfaces/book.interfaces.ts
@@ -1,4 +1,4 @@
-import { Document, model, Schema } from "mongoose";
+import { Document } from "mongoose";
 
 export interface IBook extends Document {
   title: string;
